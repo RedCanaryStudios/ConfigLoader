b0467b5b25384678e1e1f66f602156f8904fb7fa
Red Canary Studio <RedCanaryStudios@gmail.com>
2021-03-10 18:58:05 -0800

    Update configLoad.lua

--- configLoad.lua
+++ configLoad.lua
@@ -1,31 +1,3 @@
-_G.Instructions = [[
-
-   ©2021 Red Canary Studios
-   
-    -- // discord.canary.red \\ --
-
-    Usage:
-        
-        Config Loader will load configuration files into tables you can edit.
-        
-        local t = configLoader.LoadConfig(fileLocation)
-        
-    Notes:
-    
-        There may be subfolders inside the configuration.
-        Subfolders can go inside other subfolders.
-        Config folders should only contain ValueBase (BoolValue, Vector3Value, etc.) objects.
-        Any other objects will be ignored.
-
-        Folders and subfolders have a ._FOLDER property. This will be a pointer to the actual folder you're refrencing.
-
-        EG: t.subfolder._FOLDER
-            t._FOLDER
-
-]]
-
-
-
 local configLoader = {}
 
 configLoader.Cache = {}
